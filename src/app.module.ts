3100c647fba98fae535dacd90b7e3fddab640c4a
orriannafizz <orriannafizz@gmail.com>
2024-03-19 18:17:56 +0800

    feat: add redis module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RedisModule } from '@nestjs-modules/ioredis';
+import { PrismaService } from './shared/prisma/prisma.service';
 
 @Module({
-  imports: [],
+  imports: [
+    RedisModule.forRoot({
+      type: 'single',
+      options: {
+        host: 'localhost',
+        port: 46379,
+        db: 0,
+      },
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
