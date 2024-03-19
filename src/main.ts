5125d8f4b115f6090751f504ef627784fb09e014
orriannafizz <orriannafizz@gmail.com>
2024-03-19 18:07:37 +0800

    docs: set up swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const globalPrefix = 'api';
+
+  // Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Aha backend API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(`${globalPrefix}/docs`, app, document);
+
+  // Set global prefix
+  app.setGlobalPrefix(globalPrefix);
+
+  // Start app
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
