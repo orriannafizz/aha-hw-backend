f50d689bc7fc779b2b969f640b62ce36dc91624e
orriannafizz <orriannafizz@gmail.com>
2024-03-19 18:04:54 +0800

    style: google eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'google',
   ],
   root: true,
   env: {
@@ -21,5 +22,9 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'require-jsdoc': 'off',
+    'new-cap': 'off',
+    'object-curly-spacing': 'off',
+    'indent': 'off',
   },
 };
